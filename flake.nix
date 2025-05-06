3632fb9c1734095cc54c066c9177398675320006
Stefan Boca <stefan.r.boca@gmail.com>
2025-05-06 16:32:45 -0700

    Revert "feat: compact lockfile"
    This reverts commit ff5670cc7d69cb3905216e996d6e82b07b89097e.

--- flake.nix
+++ flake.nix
@@ -1,148 +1,52 @@
 {
-  description = "Home Manager configuration of doctorwho"; # TODO: change
+  description = "Home Manager configuration of doctorwho";
 
   inputs = {
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
-    };
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # TODO: remove on nixos
-    nixGL = {
-      url = "github:nix-community/nixGL";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    nixGL.url = "github:nix-community/nixGL";
+    nixGL.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix = {
-      url = "github:danth/stylix";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        flake-utils.follows = "flake-utils";
-        git-hooks.follows = "git-hooks";
-        home-manager.follows = "home-manager";
-        nixpkgs.follows = "nixpkgs";
-        nur.follows = "nur";
-        systems.follows = "systems";
-      };
-    };
-    catppuccin-fish = {
-      url = "github:catppuccin/fish";
-      flake = false;
-    };
+    stylix.url = "github:danth/stylix";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
+    stylix.inputs.home-manager.follows = "home-manager";
+    catppuccin-fish.url = "github:catppuccin/fish";
+    catppuccin-fish.flake = false;
     # TODO: remove once https://github.com/folke/tokyonight.nvim/pull/716 is merged, and use pkgs.vimPlugins.tokyonight-nvim instead
-    tokyonight-nvim = {
-      url = "github:stefanboca/tokyonight.nvim";
-      flake = false;
-    };
+    tokyonight-nvim.url = "github:stefanboca/tokyonight.nvim";
+    tokyonight-nvim.flake = false;
 
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-    # TODO: use nixpkgs ghostty on 1.1.4 release; nightly for now because of strange goto_split behavior
-    ghostty = {
-      url = "github:ghostty-org/ghostty/b6f338065eb45935627a51a7d9102019aaeae1c0";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        flake-utils.follows = "flake-utils";
-        nixpkgs-unstable.follows = "nixpkgs";
-      };
-    };
-    neovim-nightly-overlay = {
-      url = "github:nix-community/neovim-nightly-overlay";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        flake-parts.follows = "flake-parts";
-        git-hooks.follows = "git-hooks";
-        hercules-ci-effects.follows = "hercules-ci-effects";
-        nixpkgs.follows = "nixpkgs";
-        treefmt-nix.follows = "treefmt-nix";
-      };
-    };
-    zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    firefox-nightly = {
-      url = "github:nix-community/flake-firefox-nightly";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        lib-aggregate.follows = "lib-aggregate";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    emmylua-analyzer-rust = {
-      # NOTE: pinned to avoid recompiling, needs manual update
-      url = "github:EmmyLuaLs/emmylua-analyzer-rust/011fad400c9476f1dab27b5be18a6f2ed23378d9";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
+    # TODO: use nixpkgs ghostty on 1.1.4 release
+    # use nightly for now because of strange goto_split behavior
+    ghostty.url = "github:ghostty-org/ghostty/ff536e34d6cf963596dcad42d544a533bf476aac";
+    ghostty.inputs.nixpkgs-unstable.follows = "nixpkgs";
 
-    # the following are not used explicitly; we use them to compact the lockfile.
-    flake-compat.url = "github:nix-community/flake-compat";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs = {
-        flake-compat.follows = "flake-compat";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    hercules-ci-effects = {
-      url = "github:hercules-ci/hercules-ci-effects";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    lib-aggregate = {
-      url = "github:nix-community/lib-aggregate";
-      inputs = {
-        flake-utils.follows = "flake-utils";
-        nixpkgs-lib.follows = "nixpkgs-lib";
-      };
-    };
-    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
-    nur = {
-      url = "github:nix-community/nur";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        nixpkgs.follows = "nixpkgs";
-        treefmt-nix.follows = "treefmt-nix";
-      };
-    };
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-nightly-overlay.inputs.flake-parts.follows = "flake-parts";
+
+    # NOTE: pinned to avoid recompiling, needs manual update
+    emmylua-analyzer-rust.url = "github:EmmyLuaLs/emmylua-analyzer-rust/9e681265e467fd434a0e19653d0823e1116a5240";
+    emmylua-analyzer-rust.inputs.nixpkgs.follows = "nixpkgs";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    zen-browser.url = "github:youwen5/zen-browser-flake";
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    firefox-nightly.url = "github:nix-community/flake-firefox-nightly";
+    firefox-nightly.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
