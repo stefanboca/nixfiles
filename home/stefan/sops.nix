3c401cc3711725bdaaac42c4da95bcee6c984831
Stefan Boca <stefan.r.boca@gmail.com>
2025-05-07 16:50:06 -0700

    feat: sops things

--- home/stefan/sops.nix
+++ home/stefan/sops.nix
@@ -6,6 +6,14 @@
     age.sshKeyPaths = lib.mkDefault [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
 
     secrets = {
+      nix-access-tokens = { };
+      ssh-extra-config = { };
     };
   };
+
+  nix.extraOptions = ''
+    !include ${config.sops.secrets.nix-access-tokens.path}
+  '';
+
+  programs.ssh.includes = [ config.sops.secrets.ssh-extra-config.path ];
 }
