d02708e367e0b189731084c5f389ad1ca0f7750c
Stefan Boca <stefan.r.boca@gmail.com>
2025-05-21 09:23:25 -0700

    feat: secrets.nix

---
+++ hosts/common/secrets.nix
@@ -0,0 +1,19 @@
+{
+	"data": "ENC[AES256_GCM,data:H8tPB+2xv7OnXdEWuGgTImCihTaC3vuSpDb/dmYEWFes/YCec9pWAhS6zMD9LwlLMGeuEmcsPAf2PB0frmhvgDPyR7SICEuCDlHqu3D7sI0UtyjVwfj2N76GZzDTVigzI9AGkooRAiOPytpmJFsddFttGBITQN0yzCzSYfIQcha6Mf/e44Z9Z1NdJlBSM0mNws0zuboF9lAC4np43+FksRLNVZkZLnpgjVN0l+/jOd7Vd7AQzfpui9FmxRlmHkpXwf5OcFCKGhh841ZJqrroT67uvouCfLrWMaDzpb2xxvQ0be4LF+AoEUYKn4wgA2jqMrQoPUfET8itto5o3+nNllddvdqOM82sX+hP3uUc8CI9bxmfEhiDMUkH3bZ2MYDXCXTIRPREt/iCRC4Eclvz+JFZwtlDlWzrSaktXiGq5JbEZWl7lZhhQpBDx6omNr49z4PfzZNLqp7Lm4h0+wYN5OK377UO7+MVmL2REA==,iv:C7pvoOx9OabPf9f+pCaaIQjzTuY2z2XUTxEQBe07mj0=,tag:xBSgAEnC0GjkNxKJcK7W6g==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"recipient": "age1unw3p2hvz2azffj2kv45f2tl9tty4g4ggz9w4ga9hv5tnfx5xses3txuan",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB4OTE3YVhDNlhCRnlYTzAx\nRGtJK0F2dkNNUEprVFlnbExmTitSektUWHhzClhZUkdOajRIeHo3YXhLK3pOTWlv\nNUR1NGxpYTd4SENsVlU4UE00QVRlZzQKLS0tIFhtU1VBKzdraWpaMU5lWlRHKzM5\na2U2Tmh5ZmlHb2xRb3BsVytXZWovMEEKKzLZgS0557HLtyqL7lfaJEpynSVzw6kI\n0tsNbFethVEABG/DJo4pY5gxq+4kKxYjw84CI5NvIXi/IOVTb+N4rg==\n-----END AGE ENCRYPTED FILE-----\n"
+			},
+			{
+				"recipient": "age1sq7946evx8tx62vlukjyt5tvkqufkckff6mze8dgawvf82vnccjqp9dpxa",
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBCT1JEZExNY3BwY0RiN3FG\nL0ViVVlPZEhzU2NZSmk3MkJYRUFkaUlLWkRRCnhLdDcyYjUvbFkzbHhTcHU0Q0FT\naUpRZGFlaWVzYnRlK0NMbEFkd2ZzN0kKLS0tIGFWWlcyVVhhRENPOElvNURGZFNK\nREY2eU13cDJTaHhxQ2x2NWQxOW1BelEKAQib3LyZF27E9AM8OSbGCHh/ziwc6gLD\ngR01ai+Z1GuhVW+SPZbB+SuGqghsyY4kqchOF7iyQk6n68+hMWNDBA==\n-----END AGE ENCRYPTED FILE-----\n"
+			}
+		],
+		"lastmodified": "2025-05-22T00:52:52Z",
+		"mac": "ENC[AES256_GCM,data:iSg6Neeip7Plx2uat2lAKYznshUzGWyte+WNG1JR1XY0O0dH0KIKDhvyCmOhCYIip8Xe5O7NpIvTN/cQSR+5/5MG4i/6rAYEDYoa+0FAIsLphHwHCvJQ60Im7Av/MuRcFuGUPoLdvMe18Fb3q7CNsYFa0jbWBiZbB6ojEC0lJ0g=,iv:ZNRZvBeHngjjfQsmxp/fkSQCHhz7BfYvzJ4PrPztF6M=,tag:FU6ka8DNrdOnQgZlakOZFQ==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.10.2"
+	}
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 
 {
+  nix-plugins = pkgs.nix-plugins.overrideAttrs (_: {
+    buildInputs = with pkgs; [
+      nix
+      boost
+    ];
+    patches = [ ./nix-plugins.patch ];
+  });
 }
