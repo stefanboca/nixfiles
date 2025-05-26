2a299235bdf43d3319477d76c1351d5206d2f7e8
Stefan Boca <stefan.r.boca@gmail.com>
2025-05-26 14:26:43 -0700

    wip: centerpiece

---
+++ modules/home-manager/desktop/wm/default.nix
@@ -0,0 +1,18 @@
+{ inputs, ... }:
+
+{
+  imports = [ inputs.centerpiece.hmModules."x86_64-linux".default ];
+  programs.centerpiece = {
+    enable = true;
+    config = {
+      plugin = {
+        brave_bookmarks.enable = false;
+        brave_history.enable = false;
+        brave_progressive_web_apps.enable = false;
+        git_repositories.enable = false;
+        sway_windows.enable = false;
+      };
+    };
+    services.index-git-repositories.enable = false;
+  };
+}
