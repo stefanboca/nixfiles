aea6e00c1b96dac9e0d683ec9a9faf4547f64f4e
Stefan Boca <stefan.r.boca@gmail.com>
2025-06-18 21:20:53 -0700

    feat: remove gnome dependency from xdg-desktop-portal-gtk

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -10,6 +10,17 @@
         spicePkgs = inputs.spicetify-nix.legacyPackages.${final.system};
       };
 
+      overrides = final: prev: {
+        # remove gnome from dependencies
+        xdg-desktop-portal-gtk = prev.xdg-desktop-portal-gtk.overrideAttrs (prevAttrs: {
+          buildInputs = builtins.filter (
+            x: x != prev.gnome-desktop && x != prev.gnome-settings-daemon
+          ) prevAttrs.buildInputs;
+          mesonFlags = [ "-Dwallpaper=disabled" ];
+        });
+
+      };
+
       inherit (inputs.niri.overlays) niri;
       fenix = inputs.fenix.overlays.default;
       ghostty = inputs.ghostty.overlays.default;
