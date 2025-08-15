3f92de3d07864e2b6610c708222db235421531d6
Stefan Boca <stefan.r.boca@gmail.com>
2025-08-14 21:27:48 -0700

    refactor: move mutter experimental features to gnome module

--- home/stefan/laptop.nix
+++ home/stefan/laptop.nix
@@ -115,20 +115,11 @@ in
 
   dconf = {
     enable = true;
-    settings = {
-      # map screenpad touchscreen to correct display
-      "org/gnome/desktop/peripherals/touchscreens/04f3:2f2a".output = [
-        "BOE"
-        "0x0a8d"
-        "0x00000000"
-      ];
-      "org/gnome/mutter".experimental-features = [
-        "autoclose-xwayland"
-        "kms-modifiers"
-        "scale-monitor-framebuffer"
-        "variable-refresh-rate"
-        "xwayland-native-scaling"
-      ];
-    };
+    # map screenpad touchscreen to correct display in gnome
+    settings."org/gnome/desktop/peripherals/touchscreens/04f3:2f2a".output = [
+      "BOE"
+      "0x0a8d"
+      "0x00000000"
+    ];
   };
 }
---
+++ modules/home-manager/desktop/wm/gnome.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.desktop.wm;
+in
+{
+  options.desktop.wm.enableGnome = lib.mkEnableOption "Gnome DE";
+
+  config = lib.mkIf cfg.enableGnome {
+    dconf = {
+      enable = true;
+      settings."org/gnome/mutter".experimental-features = [
+        "autoclose-xwayland"
+        "kms-modifiers"
+        "scale-monitor-framebuffer"
+        "variable-refresh-rate"
+        "xwayland-native-scaling"
+      ];
+    };
+  };
+}
