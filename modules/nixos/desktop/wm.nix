879d438020fc9895f75e582a3e61af3f05894264
Stefan Boca <stefan.r.boca@gmail.com>
2025-11-25 18:35:13 -0800

    niri: use soteria instead of gnome polkit agent

--- modules/nixos/desktop/wm.nix
+++ modules/nixos/desktop/wm.nix
@@ -62,22 +62,8 @@ in {
         package = pkgs.niri-unstable;
       };
 
-      systemd.user.services = {
-        niri-flake-polkit.enable = false;
-        polkit-gnome-authentication-agent-1 = {
-          description = "polkit-gnome-authentication-agent-1";
-          wantedBy = ["graphical-session.target"];
-          wants = ["graphical-session.target"];
-          after = ["graphical-session.target"];
-          serviceConfig = {
-            Type = "simple";
-            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-            Restart = "on-failure";
-            RestartSec = 1;
-            TimeoutStopSec = 10;
-          };
-        };
-      };
+      security.soteria.enable = true;
+      systemd.user.services.niri-flake-polkit.enable = false;
 
       services = {
         # avahi.enable = true;
