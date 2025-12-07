6138cb62feff6e33e54d3635b0319aacde1406e1
Stefan Boca <stefan.r.boca@gmail.com>
2025-12-06 23:16:06 -0800

    hjem/presets: init ssh

---
+++ modules/hjem/presets/programs/ssh.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.presets.programs.ssh;
+in {
+  options.presets.programs.ssh = {
+    enable = mkEnableOption "ssh preset";
+  };
+
+  config.rum.programs.ssh = mkIf cfg.enable {
+    files = {
+      ".ssh/id_ed25519.pub".source = ../../../../home/stefan/keys/id_ed25519.pub;
+      ".ssh/id_ed25519_git.pub".source = ../../../../home/stefan/keys/id_ed25519_git.pub;
+    };
+
+    rum.programs.ssh = {
+      enable = true;
+      settings =
+        # ssh_config
+        ''
+          Host github.com
+            IdentitiesOnly yes
+            User git
+            HostName github.com
+            IdentityFile ~/.ssh/id_ed25519_git
+
+
+          Host *
+            ForwardAgent no
+            AddKeysToAgent no
+            UserKnownHostsFile ~/.ssh/known_hosts
+            ControlPath ~/.ssh/master-%r@%n:%p
+            ControlPersist no
+        '';
+    };
+  };
+}
