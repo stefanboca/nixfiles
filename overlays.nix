208c13cc91a0a1081d42681bf853eab4152ff52d
Stefan Boca <stefan.r.boca@gmail.com>
2025-12-08 12:07:06 -0800

    remove overrides
    It's like, 5MB total. Not worth the build time.

--- overlays.nix
+++ overlays.nix
@@ -1,18 +1,6 @@
 inputs: {
   default = _final: prev: import ./pkgs prev;
 
-  overrides = _final: prev: {
-    # remove gnome from dependencies
-    xdg-desktop-portal-gtk = prev.xdg-desktop-portal-gtk.overrideAttrs (prevAttrs: {
-      buildInputs = builtins.filter (x: x != prev.gnome-desktop) prevAttrs.buildInputs;
-      mesonFlags = ["-Dwallpaper=disabled"];
-    });
-
-    # use latest version of nix, to avoid having multiple versions in store
-    nixos-option = prev.nixos-option.override {nix = prev.nixVersions.latest;};
-    comma = prev.comma.override {nix = prev.nixVersions.latest;};
-  };
-
   dank-material-shell = final: prev: let
     inherit (final.stdenv.hostPlatform) system;
   in {
