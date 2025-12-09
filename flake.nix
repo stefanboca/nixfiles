289caa3f1dc4cf4b8f5df7a09d5c1cf4315feca6
Stefan Boca <stefan.r.boca@gmail.com>
2025-12-09 14:08:47 -0800

    remove home-manager input

--- flake.nix
+++ flake.nix
@@ -13,9 +13,6 @@
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
