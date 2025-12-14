05f055277f76ee5ca4c36f357797bd9b4ccff408
stefan <stefan.r.boca@gmail.com>
2025-12-14 15:37:32 -0800

    catppuccin/limine: fix: strip leading # from colors

--- modules/nixos/catppuccin/limine.nix
+++ modules/nixos/catppuccin/limine.nix
@@ -1,14 +1,16 @@
+# customize limine module to avoid IFD
 {catppuccinLib}: {
   config,
   lib,
   ...
 }: let
   inherit (catppuccinLib) mkCatppuccinOption;
+  inherit (lib.attrsets) mapAttrs;
   inherit (lib.modules) mkIf;
-  inherit (lib.strings) concatStringsSep;
+  inherit (lib.strings) concatStringsSep substring;
 
   isDark = cfg.flavor != "latte";
-  palette = config.catppuccin.palette.${cfg.flavor}.colors;
+  palette = mapAttrs (_: color: substring 1 6 color.hex) config.catppuccin.palette.${cfg.flavor}.colors;
 
   cfg = config.catppuccin.limine;
 in {
@@ -17,28 +19,28 @@ in {
   config.boot.loader.limine.style = mkIf cfg.enable {
     wallpapers = [];
     graphicalTerminal = {
-      background = palette.base.hex;
-      foreground = palette.text.hex;
+      background = palette.base;
+      foreground = palette.text;
       brightBackground =
         if isDark
-        then palette.surface2.hex
-        else palette.surface0.hex;
-      brightForeground = palette.text.hex;
+        then palette.surface2
+        else palette.surface0;
+      brightForeground = palette.text;
 
-      palette = concatStringsSep ";" [palette.base.hex palette.red.hex palette.green.hex palette.yellow.hex palette.blue.hex palette.pink.hex palette.teal.hex palette.text.hex];
+      palette = concatStringsSep ";" [palette.base palette.red palette.green palette.yellow palette.blue palette.pink palette.teal palette.text];
       brightPalette = concatStringsSep ";" [
         (
           if isDark
-          then palette.surface2.hex
-          else palette.subtext0.hex
+          then palette.surface2
+          else palette.subtext0
         )
-        palette.red.hex
-        palette.green.hex
-        palette.yellow.hex
-        palette.blue.hex
-        palette.pink.hex
-        palette.teal.hex
-        palette.text.hex
+        palette.red
+        palette.green
+        palette.yellow
+        palette.blue
+        palette.pink
+        palette.teal
+        palette.text
       ];
     };
   };
