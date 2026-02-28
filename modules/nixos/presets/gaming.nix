2397d6ec7d464787e76a59ad2feaa9fbc7a0d8f9
stefan <stefan.r.boca@gmail.com>
2026-02-27 21:26:47 -0800

    disable gaming specialisation for now

--- modules/nixos/presets/gaming.nix
+++ modules/nixos/presets/gaming.nix
@@ -26,64 +26,64 @@ in {
       };
     };
 
-    specialisation.gaming.configuration = {
-      environment.etc."specialisation".text = "gaming";
-      system.nixos.tags = ["gaming"];
-
-      boot = {
-        kernel.sysctl = {"vm.swappiness" = 10;};
-        kernelParams = ["threadirqs"];
-        kernelPackages = mkForce pkgs.linuxKernel.packages.linux_zen;
-      };
-
-      security.pam.loginLimits = [
-        {
-          domain = "@audio";
-          item = "memlock";
-          type = "-";
-          value = "unlimited";
-        }
-        {
-          domain = "@audio";
-          item = "rtprio";
-          type = "-";
-          value = "99";
-        }
-        {
-          domain = "@audio";
-          item = "nofile";
-          type = "soft";
-          value = "99999";
-        }
-        {
-          domain = "@audio";
-          item = "nofile";
-          type = "hard";
-          value = "99999";
-        }
-      ];
-
-      hardware.nvidia.powerManagement.finegrained = mkForce false;
-
-      services = {
-        pipewire = {
-          jack.enable = true;
-          extraConfig.pipewire."92-low-latency" = {
-            "context.properties" = {
-              "default.clock.rate" = 48000;
-              "default.clock.allowed-rates" = [44100 48000 96000];
-              "default.clock.quantum" = 128;
-              "default.clock.min-quantum" = 16;
-              "default.clock.max-quantum" = 1024;
-            };
-          };
-        };
-        scx = {
-          enable = true;
-          scheduler = "scx_lavd";
-          package = pkgs.scx.rustscheds;
-        };
-      };
-    };
+    # specialisation.gaming.configuration = {
+    #   environment.etc."specialisation".text = "gaming";
+    #   system.nixos.tags = ["gaming"];
+    #
+    #   boot = {
+    #     kernel.sysctl = {"vm.swappiness" = 10;};
+    #     kernelParams = ["threadirqs"];
+    #     kernelPackages = mkForce pkgs.linuxKernel.packages.linux_zen;
+    #   };
+    #
+    #   security.pam.loginLimits = [
+    #     {
+    #       domain = "@audio";
+    #       item = "memlock";
+    #       type = "-";
+    #       value = "unlimited";
+    #     }
+    #     {
+    #       domain = "@audio";
+    #       item = "rtprio";
+    #       type = "-";
+    #       value = "99";
+    #     }
+    #     {
+    #       domain = "@audio";
+    #       item = "nofile";
+    #       type = "soft";
+    #       value = "99999";
+    #     }
+    #     {
+    #       domain = "@audio";
+    #       item = "nofile";
+    #       type = "hard";
+    #       value = "99999";
+    #     }
+    #   ];
+    #
+    #   hardware.nvidia.powerManagement.finegrained = mkForce false;
+    #
+    #   services = {
+    #     pipewire = {
+    #       jack.enable = true;
+    #       extraConfig.pipewire."92-low-latency" = {
+    #         "context.properties" = {
+    #           "default.clock.rate" = 48000;
+    #           "default.clock.allowed-rates" = [44100 48000 96000];
+    #           "default.clock.quantum" = 128;
+    #           "default.clock.min-quantum" = 16;
+    #           "default.clock.max-quantum" = 1024;
+    #         };
+    #       };
+    #     };
+    #     scx = {
+    #       enable = true;
+    #       scheduler = "scx_lavd";
+    #       package = pkgs.scx.rustscheds;
+    #     };
+    #   };
+    # };
   };
 }
