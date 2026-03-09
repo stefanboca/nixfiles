ffdc8317f567cd724c0e8dcab2eb5b975c6c16c2
stefan <stefan.r.boca@gmail.com>
2026-03-09 13:16:31 -0700

    remove nvidia patch

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -53,15 +53,7 @@ in {
         nvidiaBusId = "PCI:1:0:0";
       };
 
-      # FIXME: https://github.com/NixOS/nixpkgs/pull/490123
-      package = let
-        base = config.boot.kernelPackages.nvidiaPackages.beta;
-        cachyos-nvidia-patch = pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
-          sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
-        };
-      in
-        base // {open = base.open.overrideAttrs (prevAttrs: {patches = (prevAttrs.patches or []) ++ [cachyos-nvidia-patch];});};
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
     };
   };
 
