1c131af416bd95fecf4e35753b1c4ffe1b7ff169
stefan <stefan.r.boca@gmail.com>
2026-04-30 16:46:33 -0700

    trust flox cuda cache

--- modules/nixos/presets/minimal.nix
+++ modules/nixos/presets/minimal.nix
@@ -34,8 +34,14 @@ in {
         warn-dirty = false;
         # keep-sorted end
 
-        substituters = ["https://nix-community.cachix.org"];
-        trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://cache.flox.dev"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+        ];
       };
 
       registry = {
