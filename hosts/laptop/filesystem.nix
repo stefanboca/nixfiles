b02aeba8264be7ebd9fa89421b1d868b8c658548
stefan <stefan.r.boca@gmail.com>
2026-05-21 13:33:12 -0700

    laptop: increase compression level, beesd

--- hosts/laptop/filesystem.nix
+++ hosts/laptop/filesystem.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   disko.devices.disk.main = {
     type = "disk";
     device = "/dev/nvme0n1";
@@ -21,6 +21,7 @@
           size = "100%";
           content = {
             type = "btrfs";
+            extraArgs = "--label main";
             mountpoint = "/partition-root";
             swap.swapfile.size = "64G";
             subvolumes = {
@@ -29,15 +30,15 @@
               };
               "/var" = {
                 mountpoint = "/var";
-                mountOptions = ["compress=zstd:1"];
+                mountOptions = ["compress=zstd"];
               };
               "/home" = {
                 mountpoint = "/home";
-                mountOptions = ["compress=zstd:1"];
+                mountOptions = ["compress=zstd"];
               };
               "/nix" = {
                 mountpoint = "/nix";
-                mountOptions = ["compress=zstd:1" "noatime"];
+                mountOptions = ["compress=zstd" "noatime"];
               };
             };
           };
@@ -46,20 +47,29 @@
     };
   };
 
-  services.btrbk.instances = {
-    main.settings = {
-      timestamp_format = "long";
-      snapshot_preserve_min = "4d";
-      snapshot_preserve = "14d 4w 3m";
-      snapshot_create = "onchange";
-      volume."/partition-root" = {
-        snapshot_dir = "snapshots";
-        subvolume = {
-          home = {};
-          rootfs = {};
-          var = {};
+  services = {
+    beesd.filesystems."main" = {
+      spec = "LABEL=main";
+      hashTableSizeMB = 512;
+      extraOptions = ["--throttle-factor" "1"];
+    };
+    btrbk.instances = {
+      main.settings = {
+        timestamp_format = "long";
+        snapshot_preserve_min = "4d";
+        snapshot_preserve = "14d 4w 3m";
+        snapshot_create = "onchange";
+        volume."/partition-root" = {
+          snapshot_dir = "snapshots";
+          subvolume = {
+            home = {};
+            rootfs = {};
+            var = {};
+          };
         };
       };
     };
   };
+
+  systemd.services."beesd@main".wantedBy = lib.mkForce [];
 }
