89fcf0bbc55bffd99f131dd56b8b08135e252dad
stefan <stefan.r.boca@gmail.com>
2026-09-03 17:14:35 -0700

    flake: simplify nixConfig with extra-*

--- flake.nix
+++ flake.nix
@@ -134,11 +134,8 @@
 
   nixConfig = {
     allow-import-from-derivation = false;
-    substituters = ["https://cache.nixos.org" "https://nix-community.cachix.org"];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     extra-experimental-features = ["pipe-operators"];
   };
 }
